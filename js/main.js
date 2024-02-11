5921ac6c3d21f2d94a02193d0a9f24b83be529ad
E1-XP <sebastian-stypula@hotmail.com>
2024-02-11 15:58:19 +0100

    feat: finish RWD

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 import { processAccordionInteractions } from "./accordion";
 
-import "bootstrap/dist/css/bootstrap-reboot.min.css";
-import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "./../scss/main.scss";
 
 const main = (() => {
